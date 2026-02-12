de84d89387f5b8197db2244d34b130cd58025827
Lily Cohen <lilymooncohen@proton.me>
2026-02-12 09:19:48 -0700

    Update t2linux-disable-all.sh

--- scripts/t2linux-disable-all.sh
+++ scripts/t2linux-disable-all.sh
@@ -5,7 +5,7 @@
 # 2. Deactivate WiFi interface
 /usr/bin/nmcli radio wifi off
 # 3. Unload WiFi plugin and driver
-/usr/sbin/modprobe -r brcmfmac_wcc
-/usr/sbin/modprobe -r brcmfmac
+#/usr/sbin/modprobe -r brcmfmac_wcc
+#/usr/sbin/modprobe -r brcmfmac
 # 4. Apple BCE removal
 /usr/sbin/rmmod -f apple-bce
